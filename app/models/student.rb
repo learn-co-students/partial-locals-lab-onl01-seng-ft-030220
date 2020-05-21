a5b6522257d6937bd51635dc8be8d9b2a6fd8e6d
TerrenceBell <terrencekbell@gmail.com>
2020-05-20 22:32:55 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,18 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  # def self.search(query)
+   #   query.present? ? where("NAME like ?", "%#{query}%") : self.all
+   # end 
+   def self.search(query)
+    if query.present?
+      where('NAME like ?', "%#{query}%")
+    else
+      self.all
+    end
+  end
 end
+
+
